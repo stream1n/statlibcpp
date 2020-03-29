8d39e7e6485914b769b8588885b1aff359a1b0b4
Ronan Hughes <streamin@Ronans-MacBook-Pro.local>
2020-03-29 11:56:04 +0100

    standard deviation

--- source/summary/test/SummaryStatisticsTest.cpp
+++ source/summary/test/SummaryStatisticsTest.cpp
@@ -5,7 +5,9 @@ const std::vector<int> vec_even = {9, 2, 1, 4, 5, 8, 6, 7, 3, 10};
 
 const std::vector<int> vec_odd = {9, 2, 1, 4, 5, 8, 11, 6, 7, 3, 10};
 
-const std::vector<int> std_dev = {10, 40, 30, 50, 20};
+//const std::vector<int> std_dev = {10, 40, 30, 50, 20};
+
+const std::vector<int> std_dev = {64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060};
 
 TEST(SummaryStatisticsTestSuite, SummaryStatisticsTestSuite_Sumean_Test){
     statlib::summaries::SummaryStatistics stats = statlib::summaries::mean(vec_even);
@@ -22,6 +24,6 @@ TEST(SummaryStatisticsTestSuite, SummaryStatisticsTestSuite_Sumedian_odd_Test){
 
 TEST(SummaryStatisticsTestSuite, SummaryStatisticsTestSuite_Sustandard_deviation_Test){
     statlib::summaries::SummaryStatistics stats = statlib::summaries::standard_deviation(std_dev);
-    EXPECT_DOUBLE_EQ(stats.getProperties().getMean(), 30);
-    EXPECT_NEAR(stats.getProperties().getStandardDeviation(), 14.1421, 0.001);
+    EXPECT_DOUBLE_EQ(stats.getProperties().getMean(), 43900.6);
+    EXPECT_NEAR(stats.getProperties().getStandardDeviation(), 30466.9, 0.1);
 }
